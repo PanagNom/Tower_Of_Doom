1564aaf57b4b236e93ac3242e21a0c585e8cce79
Panagiotis Nomikos <102253139+PeterNom@users.noreply.github.com>
2024-07-15 21:46:26 +0300

    Updated the code with comments.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,15 +5,15 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject Player;
-    public Vector3 LastKnownPos;
-
+    private GameObject player;
+    private Vector3 lastKnownPos;
     private NavMeshAgent m_Agent;
     private StateMachine m_StateMachine;
-    public float attackRate;
 
     public Path path;
     public NavMeshAgent Agent { get { return m_Agent; } }
+    public GameObject Player { get { return player; } }
+    public Vector3 LastKnownPos { get { return lastKnownPos; } set { lastKnownPos = value; } }
 
     [SerializeField]
     private string currentState;
@@ -22,6 +22,7 @@ public class Enemy : MonoBehaviour
     private float maxHealth = 10f;
     private float sightDistance = 10f;
     public float fieldOfView = 85f;
+    public float attackRate;
 
     // Start is called before the first frame update
     void Start()
@@ -29,9 +30,9 @@ public class Enemy : MonoBehaviour
         health = maxHealth;
         m_Agent = GetComponent<NavMeshAgent>();
         m_StateMachine = GetComponent<StateMachine>();
-        Player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
 
-        m_StateMachine.Initialise();
+        m_StateMachine.Initialize();
     }
     private void Update()
     {
@@ -39,29 +40,43 @@ public class Enemy : MonoBehaviour
     }
     public bool CanSeePlayer()
     {
+        // If the player exists.
         if(Player)
         {
+            // Check the distance between the player and the enemy.
             if (Vector3.Distance(transform.position, Player.transform.position) < sightDistance)
             {
+                // Find the direction of the player and calculate the angle the enemy has with the player direction.
                 Vector3 targetDirection = Player.transform.position - transform.position;
                 float angleToPlayer = Vector3.Angle(transform.forward, targetDirection);
 
+                // If the angle is inside the fieldOfView of the enemy.
                 if (angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView)
                 {
+                    // Create a ray from the enemy with direction to the player
                     Ray ray = new Ray(transform.position, targetDirection);
                     RaycastHit hitInfo = new RaycastHit();
 
+                    // Perform a check to see if the ray hit anything the enemy can see.
                     if (Physics.Raycast(ray, out hitInfo, sightDistance))
                     {
+                        // If it did hit and that object was the player return true that you can see the player.
                         if (hitInfo.transform.gameObject == Player)
                         {
-                            Debug.DrawRay(ray.origin, ray.direction * sightDistance);
+                            Debug.DrawRay(ray.origin, ray.direction * sightDistance*10);
                             return true;
                         }
                     }
                 }
             }
         }
+        // Return false because you can't see the player.
         return false;
+    }
+
+    private void Death()
+    {
+        Debug.Log("Enemy death.");
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Enemy/States/PatrolState.cs
+++ Assets/Scripts/Enemy/States/PatrolState.cs
@@ -7,30 +7,40 @@ public class PatrolState : BaseState
     public int waypointIndex;
     public float waitTimer;
 
+    // Function executed when we first enter the state.
     public override void Enter()
     {
     }
-
+    // Function executed when we exit the state.
     public override void Exit()
     {
     }
 
+    // Function executed every frame as long as the state is active.
     public override void Perform()
     {
+        // Move the enemy along the designated path.
         PatrolCycle();
+
+        // If the enemy can see the player, change to the attack state.
         if (enemy.CanSeePlayer())
         {
             stateMachine.ChangeState( new AttackState());
         }
     }
-
+    // Function to move the enemy along the designated path.
     public void PatrolCycle()
     {
-        if(enemy.Agent.remainingDistance < 0.2f)
+        Debug.Log(enemy.Agent.remainingDistance);
+        // If you have reached a paths node or this is the first run and the agent 
+        // does not have a destination so the remainingDistance is 0 move to the next waypoint.
+        if (enemy.Agent.remainingDistance < 0.2f)
         {
+            // Wait for a small amount of time before moving forward.
             waitTimer += Time.deltaTime;
             if (waitTimer > 3)
             {
+                // Circle around the waypoints.
                 if (waypointIndex < enemy.path.waypoints.Count - 1)
                 {
                     waypointIndex++;
@@ -39,6 +49,8 @@ public class PatrolState : BaseState
                 {
                     waypointIndex = 0;
                 }
+
+                // Set the destination of the enemy for the next waypoint.
                 enemy.Agent.SetDestination(enemy.path.waypoints[waypointIndex].position);
                 waitTimer = 0;
             }
